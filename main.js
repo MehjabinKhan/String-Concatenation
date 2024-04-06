e3a473b381d6356e3be5d574e3a0110bfc097dd5
MehjabinKhan <mehjabinkhan2004@gmail.com>
2024-04-06 21:58:35 +0500

    string concatenation

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+// String Concatenation
+let firstname = "Mehjabin";
+let lastname = "Khan";
+let fullname = firstname + ' ' + lastname;
+console.log(fullname);
+let today1sttask = "Learn string";
+let today2ndtask = "Operators";
+let todaytask = today1sttask + ' and ' + today2ndtask;
+console.log(todaytask);
+let myProject = "String Concatenation";
+let madeBy = "Mehjabin Khan";
+console.log(' Project: ' + myProject + ' made by: ' + madeBy);
+let workingOn = "Computer";
+let task = "to learn Typescript";
+console.log('I am working on working on ' + workingOn + ' ' + task);
---
+++ main.ts
@@ -0,0 +1,18 @@
+// String Concatenation
+let firstname: string = "Mehjabin";
+let lastname: string = "Khan";
+let fullname: string = firstname + ' ' + lastname;
+console.log(fullname);
+
+let today1sttask: string = "Learn string";
+let today2ndtask: string = "Operators";
+let todaytask: string = today1sttask + ' and ' + today2ndtask;
+console.log(todaytask);
+
+let myProject: string = "String Concatenation";
+let madeBy: string = "Mehjabin Khan";
+console.log(' Project: ' + myProject + ' made by: ' + madeBy);
+
+let workingOn: string = "Computer";
+let task: string = "to learn Typescript";
+console.log('I am working on working on ' + workingOn + ' ' + task);
